e4670b8ed4e315456dc6df9aa4620099a34ddac8
sergioedo <sergio.edo@gmail.com>
2024-11-10 10:40:48 +0100

    new verify token manually and configure render login button

--- src/pages/api/token/verify.js
+++ src/pages/api/token/verify.js
@@ -1,19 +1,59 @@
 import fetch from 'node-fetch';
+import jose from 'node-jose';
+
+const googleClientID = import.meta.env.PUBLIC_GOOGLE_CLIENT_ID;
 
 export async function POST({ request }) {
 	const { idToken } = await request.json();
 
-	const googleClientID = import.meta.env.PUBLIC_GOOGLE_CLIENT_ID;
-	const response = await fetch(`https://oauth2.googleapis.com/tokeninfo?id_token=${idToken}`);
-	const tokenInfo = await response.json();
-
-	if (tokenInfo.aud === googleClientID) {
-		return new Response(JSON.stringify({ message: "Autenticado exitosamente", user: tokenInfo }), {
+	try {
+		// const token = await verifyGoogleTokenAuto(idToken);
+		const token = await verifyGoogleTokenManual(idToken);
+		return new Response(JSON.stringify({ message: "Autenticado correctamente", user: token }), {
 			status: 200,
 		});
-	} else {
-		return new Response(JSON.stringify({ error: "Token inválido" }), {
+	} catch (error) {
+		return new Response(JSON.stringify({ error }), {
 			status: 401,
 		});
 	}
 }
+
+async function verifyGoogleTokenAuto(idToken) {
+	const response = await fetch(`https://oauth2.googleapis.com/tokeninfo?id_token=${idToken}`);
+	const tokenInfo = await response.json();
+	if (tokenInfo.aud === googleClientID) {
+		return tokenInfo;
+	} else {
+		throw new Error('Token inválido');
+	}
+}
+
+async function verifyGoogleTokenManual(idToken) {
+	// Paso 1: Obtener las claves públicas de Google
+	const response = await fetch('https://www.googleapis.com/oauth2/v3/certs');
+	const { keys } = await response.json();
+
+	// Paso 2: Decodificar el encabezado del token para obtener el 'kid'
+	const [header] = idToken.split('.');
+	const decodedHeader = JSON.parse(Buffer.from(header, 'base64').toString('utf8'));
+	const kid = decodedHeader.kid;
+
+	// Paso 3: Encontrar la clave pública correspondiente al 'kid'
+	const key = keys.find(key => key.kid === kid);
+	if (!key) throw new Error('No se encontró la clave pública para el token.');
+
+	// Paso 4: Verificar el token usando la clave pública
+	const keystore = jose.JWK.createKeyStore();
+	const publicKey = await keystore.add(key);
+	const verifiedToken = await jose.JWS.createVerify(publicKey).verify(idToken);
+
+	// Paso 5: Parsear el payload del token y verificar audiencia
+	const payload = JSON.parse(verifiedToken.payload.toString());
+	if (payload.aud !== googleClientID) {
+		throw new Error('Audiencia no válida.');
+	}
+
+	return payload; // Contiene la información del usuario verificada
+}
+
