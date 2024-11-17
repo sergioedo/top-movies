db7372331e622bafef80f365ec75d53ce7773d56
sergioedo <sergio.edo@gmail.com>
2024-11-17 10:35:22 +0100

    add alpinejs to manage user movies state

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,9 +4,11 @@ import vercel from '@astrojs/vercel/serverless';
 
 import tailwind from '@astrojs/tailwind';
 
+import alpinejs from '@astrojs/alpinejs';
+
 // https://astro.build/config
 export default defineConfig({
   output: 'server',
   adapter: vercel(),
-  integrations: [tailwind()]
+  integrations: [tailwind(), alpinejs()]
 });
