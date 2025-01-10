05ecb91c04f098a31b57262b2779a7d68d6a8789
sergioedo <sergio.edo@gmail.com>
2025-01-10 19:20:38 +0100

    fix className

--- src/components/YearMoviesCard.jsx
+++ src/components/YearMoviesCard.jsx
@@ -12,16 +12,16 @@ const YearMoviesCard = ({ yearMovies, totalMovies }) => {
 	};
 	const roundedCorners = ['rounded-tl-2xl', 'rounded-tr-2xl', 'rounded-bl-2xl', 'rounded-br-2xl']
 	return (
-		<div class="flex items-center justify-center transparent relative" >
+		<div className="flex items-center justify-center transparent relative" >
 			{/* Contenedor de los posters y círculo*/}
-			<div class="grid grid-cols-2 grid-rows-2 gap-0">
+			<div className="grid grid-cols-2 grid-rows-2 gap-0">
 				{
 					movies.map((movie, idx) => (
 						<a href={`/movies/${movie.id}`} key={movie.id} id={movie.id}>
 							<img
 								src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`}
 								alt={`${movie.title} Poster`}
-								class={`thumbnail hover:opacity-75 transition ease-in-out duration-150 ${roundedCorners[idx]}`}
+								className={`thumbnail hover:opacity-75 transition ease-in-out duration-150 ${roundedCorners[idx]}`}
 							/>
 						</a>
 					))
