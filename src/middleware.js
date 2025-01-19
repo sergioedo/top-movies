0a79081500360e0bd77be09571f34a07e14a67b0
sergioedo <sergio.edo@gmail.com>
2025-01-19 10:57:23 +0100

    Next list requires login

--- src/middleware.js
+++ src/middleware.js
@@ -3,6 +3,7 @@ import { getSession } from 'auth-astro/server';
 const isProtectedPath = (path) => {
 	//API: /api, excepts /api/auth (public validation)
 	if (path.startsWith('/api') && !path.startsWith('/api/auth')) return true;
+	// Admin page
 	if (path.startsWith('/admin')) return true;
 	return false;
 }
@@ -12,16 +13,13 @@ export async function onRequest(context, next) {
 	const urlObject = new URL(context.url);
 	const path = urlObject.pathname;
 
-	if (isProtectedPath(path)) {
-		const session = await getSession(context.request);
-		if (!session) {
-			return new Response("Unauthorized: Missing or invalid session", { status: 401 });
-		}
-
+	const session = await getSession(context.request);
+	if (isProtectedPath(path) && !session) {
+		return new Response("Unauthorized: Missing or invalid session", { status: 401 });
+	}
+	if (session) {
 		//TODO: validate expiration, renew...
 		context.locals.user = session.user;
-		return next();
-	} else {
-		return next();
 	}
+	return next();
 }
